67c047a54967be9dca8c6db34fc69155ef601589
Maximilian Wolf <github@wolf-max.com>
2024-07-09 14:15:29 +0200

    feat: raised some dependency versions

--- README.md
+++ README.md
@@ -27,6 +27,9 @@ This project is a Rust-based server running on an ESP32 microcontroller, designe
 - **ESP-IDF:** Install the ESP-IDF development environment. Follow the [ESP-IDF Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/index.html).
 - **BME280 Sensor:** Connect a BME280 sensor to your ESP32 development board.
 
+## Configuration
+The `config.rs` contains various constants used for configuring e.g. `OVERSAMPLE` and `MEASUREMENT_DELAY`.
+
 ## Usage
 Once deployed, the server will begin reading data from the BME280 sensor. Future updates will include MQTT support and a Homebridge plugin for smart home integrations.
 
--- src/config.rs
+++ src/config.rs
@@ -2,23 +2,23 @@ use bme280::Oversampling;
 use bme280::SensorMode;
 use std::time::Duration;
 
-// TODO: Remove unused config or introduce ways to disable
+// TODO: Introduce SENSOR_MODE, TEMPERATURE-, HUMIDITY- and PRESSURE_MEASURING feature toggles.
 
 /// Sensor Mode used for measuring. Normal is measuring at regular times,
 /// Forced only once and then enters Sleep.
-pub static SENSOR_MODE: SensorMode = SensorMode::Normal;
+pub static _SENSOR_MODE: SensorMode = SensorMode::Normal;
 
 // Oversampling mode
 pub static GLOBAL_OVERSAMPLING: Oversampling = Oversampling::Oversampling2X;
 
 /// Enable Temperature measurement.
-pub static TEMPERATURE_MEASURING_ENABLED: bool = true;
+pub static _TEMPERATURE_MEASURING_ENABLED: bool = true;
 
 /// Enable Humidity measurement.
-pub static HUMIDITY_MEASURING_ENABLED: bool = true;
+pub static _HUMIDITY_MEASURING_ENABLED: bool = true;
 
 /// Enable Pressure measurement.
-pub static PRESSURE_MEASURING_ENABLED: bool = true;
+pub static _PRESSURE_MEASURING_ENABLED: bool = true;
 
 static MEASUREMENT_DELAY_SECONDS: u64 = 1;
 static MEASUREMENT_DELAY_MILLIS: u32 = 0;
